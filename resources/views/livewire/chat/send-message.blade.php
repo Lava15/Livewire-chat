f49517fda7c92beaa2b1767b784bca47a7eb9bac
LA <lava9336@gmail.com>
2022-08-30 15:56:58 +0500

    syncronized

--- resources/views/livewire/chat/send-message.blade.php
+++ resources/views/livewire/chat/send-message.blade.php
@@ -1,10 +1,11 @@
 <div>
-    <form action="" wire:submit.prevent="send" >
+    <form action="#" wire:submit.prevent="send">
+        <span x-text="typing"></span>
+
         <div>
             <textarea class="w-1/2 text-black rounded-lg" rows="10" wire:model="body"></textarea>
         </div>
-            {{ $body }}
-        <button class="bg-white text-blue-600 w-1/2 my-5 rounded-lg p-2" type="submit">
+        <button class="bg-white text-blue-600 w-1/2 my-5 rounded-lg p-2" x-ref="submit" type="submit">
             Send
         </button>
     </form>
